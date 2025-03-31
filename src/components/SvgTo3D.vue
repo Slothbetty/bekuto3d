eb192b3e01ecf21f40ae0e5f69f9ea48ac650b8f
Rizumu Ayaka <rizumu@ayaka.moe>
2025-03-31 15:21:18 +0800

    fix: update the default SVG depth list to optimize rendering effects

--- src/components/SvgTo3D.vue
+++ src/components/SvgTo3D.vue
@@ -322,7 +322,7 @@ const DEFAULT_SVG = '/model/bekuto3d.svg'
 const isDefaultSvg = computed(() => fileName.value === 'default-bekuto3d.svg')
 
 const defaultSvgOffsetList = [0, 2.1]
-const defaultSvgDepthList = [2.1, 0, 3, 2, 2, 4, 2, 2, 2]
+const defaultSvgDepthList = [2.1, 0, 1, 1, 1, 2, 1, 1.4, 1.6]
 // 添加加载默认文件的函数
 async function loadDefaultSvg() {
   try {
